f1eda02688deb15c197e4922ced8cdf9c57a6411
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-09-18 17:34:05 +0000

    Added area and perimeter of a rectangle with a length of 5cm and width of 3cm

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Nathan Araujo
+# Created on: Sept, 16 2022
+# This program does basic math.
+
+def main():
+    print ("7.9 + 5.4 = {:.2f}". format(7.9+5.4))
+    print ("3.5 - 6.2 = {:.2f}". format(3.5-6.2))
+    print ("100 / 10 = {}". format(100/10))
+    print ("4 + 10 / 2 = {}". format(4+10/2))
+    print ("3^5 = {}". format(3**5))
+    print ("2 / 5 = {}". format(2/5))
+
+if __name__ == "__main__":  
+  main()
